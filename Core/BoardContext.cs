0000f14b8d23f3e44f86b728a3ab942974683b82
DarckList <sashagigabyte@gmail.com>
2024-07-18 17:40:49 +0300

    added tokens to board context

--- Core/BoardContext.cs
+++ Core/BoardContext.cs
@@ -1,10 +1,12 @@
 ﻿using Core.Components.DevelopmentCards;
 using Core.Components.Nobels;
 using Core.Components.Players;
+using Core.Components.Tokens;
 
 namespace Core;
 internal class BoardContext
 {
+	public List<Token> Tokens { get; init; } = new();
 	public List<Player> Players { get; init; } = new();
 	public List<Nobel> Nobels { get; init; } = new();
 	public Dictionary<DevelopmentCardlevel, Stack<DevelopmentCard>> DevelopmentCards { get; init; } = new();
