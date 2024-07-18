401b6327f1ec9ba114c8c3b9b0a838677bac59d2
DarckList <sashagigabyte@gmail.com>
2024-07-18 17:40:12 +0300

    removed unnecessary code

--- Core/Components/Tokens/Token.cs
+++ Core/Components/Tokens/Token.cs
@@ -1,49 +1,29 @@
-﻿using System.Drawing;
-
-namespace Core.Components.Tokens;
-public class Token : IRendable
+﻿namespace Core.Components.Tokens;
+public class Token
 {
 
-    public TokenType Type { get; private set; }
-    public int Count { get; private set; }
+	public TokenType Type { get; private set; }
+	public int Count { get; private set; }
 
+	public Token(TokenType type, int ammount)
+	{
+		Type = type;
+		Count = ammount;
+	}
 
-    public ConsoleColor TokenColor { get; private set; }
+	public void SpendTokens(int price)
+	{
+		Count -= price;
 
-    public Token(TokenType type, int ammount, ConsoleColor tokenColor)
-    {
-        Type = type;
-        Count = ammount;
-        TokenColor = tokenColor;
-    }
+	}
 
-    public void SpendTokens(int price)
-    {
-        Count -= price;
-
-    }
-
-    public void IncreaseTokens(int ammount)
-    {
-        if (ammount <= 0)
-        {
-            Console.WriteLine("ammount shoud be positiv value");
-            return;
-        }
-        Count += ammount;
-    }
-
-    public void Render()
-    {
-        var previewColor = Console.ForegroundColor;
-        Console.ForegroundColor = TokenColor;
-        Console.Write($"{RenderType()}-{Count}, ");
-        Console.ForegroundColor = previewColor;
-    }
-
-
-    private string RenderType()
-    {
-        return Type.ToString();
-    }
+	public void IncreaseTokens(int ammount)
+	{
+		if (ammount <= 0)
+		{
+			Console.WriteLine("ammount shoud be positiv value");
+			return;
+		}
+		Count += ammount;
+	}
 }
